880882ea7c018eeadcde93f77b4bd9a3992b6f15
Ivethpaz <112119939+Ivethpaz@users.noreply.github.com>
2022-09-12 22:16:26 -0600

    Add files via upload

---
+++ lab02-celcius2farenheit.py
@@ -0,0 +1,7 @@
+#  Tendencias e Innovacion en Tecnologia Agricola (TEA)
+celcius= float(input("Temperatura en Celcius"))
+fahrenheit= 9/5 input(celcius) * 32 
+
+input= (int(fahrenheit))
+
+
---
+++ lab02-pagoporhora.py
@@ -0,0 +1,9 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+horas = input("Horas trabajadas?") 
+print("Horas trabajadas" + horas)
+
+preciohora= input ("Precio por hora?")
+print ("Precio por hora" + preciohora)
+
+Calculo= int(horas) * int(preciohora)
+print (Calculo)
